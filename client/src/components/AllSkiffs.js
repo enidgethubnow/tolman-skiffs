bcc1e78f8369192b42699f2fe0a1fb9615f6c842
Your NameOrlandofl57 <enidsoftwaredevelopment@gmail.com>
2021-03-24 02:36:24 -0700

    added image to skiff

--- client/src/components/AllSkiffs.js
+++ client/src/components/AllSkiffs.js
@@ -3,11 +3,40 @@ import axios from 'axios';
 import {Link, navigate} from '@reach/router';
 
 const AllSkiffs = (props) => {
+    const [allSkiffs, setAllSkiffs] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get("http://localhost:8000/api/skiffs")
+        .then((res) =>{
+            console.log(res.data);
+            setAllSkiffs(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+        
+
+    },[]);
+
     return(
     <div>
         <h2>All Skiffs</h2>
-    </div>
-    )
+        {
+            allSkiffs.map((skiff,index) => (
+                <div> 
+                    <img
+                    src={ skiff.pictureUrl }
+                    alt= {skiff.description}
+                    />
+                    <h4>{`skiff.ownerName}'s  ${skiff.modelName} Skiff`}</h4>
+                    <button
+                    onClick ={() => navigate(`/skiff/${skiff._id}`)}
+                    >View Details</button>
+                    <button>Titanic</button>
+                </div>
+    ))
 }
-
+</div>
+    )}
 export default AllSkiffs;
